925720267482155bf30a08be3045e29a98ecf2be
ze25800000 <ze258100000@sina.com>
2016-11-21 17:08:33 +0800

    添加了一些选项卡功能

--- js/index.js
+++ js/index.js
@@ -1,47 +1,29 @@
-$('.t1').click(function () {
-    $(this).addClass('on');
-    $('.t2').removeClass('on');
-    $('.newest').css('display', 'block');
-    $('.hottest').css('display', 'none');
-});
-$('.t2').click(function () {
-    $(this).addClass('on');
-    $('.t1').removeClass('on');
-    $('.newest').css('display', 'none');
-    $('.hottest').css('display', 'block');
-
-});
 /*****************information*****************/
 function change(element) {
     var Lis = $('.'+element+' .head-title li');
     var hContent = $('.' + element + ' .h-content');
-   /* for (var i=0;i<Lis.length;i++) {
-     (function (index) {
-     Lis[index].onclick = function () {
-     for (var i = 0; i < Lis.length; i++) {
-     Lis[i].className = i == index ? 'on' : '';
-     hContent[i].style.display = i == index ? 'block' : 'none';
-     }
-     };
-     /!* $('.'+element+' .head-title li:eq('+i+')').click(function () {
-     for (var i=0;i<Lis.length;i++) {
-     if (i == index) {
-     $('.'+element+' .head-title li:eq('+i+')').addClass('on');
-     $('.' + element + ' .h-content:eq(' + i + ')').css('display', 'block');
-     console.log(this)
-     }else {
-     $('.'+element+' .head-title li:eq('+i+')').removeClass('on');
-     $('.' + element + ' .h-content:eq(' + i + ')').css('display', 'none');
-     }
-     }
-     })*!/
-     })(i)
-     }*/
     Lis.click(function () {
        $(this).addClass('on').siblings().removeClass('on');
+        hContent.eq($(this).index()).addClass('changetab').siblings().removeClass('changetab');
    })
 }
 change('information');
 change('actual');
 change('market');
 change('business');
+/******************artical*********************/
+$('.content .left .title h2').click(function () {
+    $(this).addClass('on').siblings().removeClass('on');
+    $('.est').eq($(this).index()).addClass('on').siblings().removeClass('on');
+});
+/*******************actual time********************/
+function time() {
+    var timer = new Date();
+    var week = { '1': '星期一', '2': '星期二', '3': '星期三', '4': '星期四', '5': '星期五', '6': '星期六', '0': '星期日' };
+    var actualTime = timer.toLocaleString().replace(/\//g, '-').replace(/[上|下]午\d{1,2}:/, timer.getHours() + ':');
+    var calendarTime = timer.toLocaleDateString().replace(/\//g, '-')+' '+week[timer.getDay()];
+    $('.actual .fixed').html('<div class="fixed"><i class="iconfont icon-shijian"></i>' + actualTime + '</div>');
+    $('.calendar .c-time').html('<span><i class="iconfont icon-shijian"></i>'+calendarTime+'</span><span>据下次数据公布时间还有:<em>1小时4分17秒</em></span>')
+}
+setInterval(time, 1000);
+/***************************************************/
