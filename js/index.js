a914f29ae07b867a8ba8447c5e583d68ef57947d
ze25800000 <ze258100000@sina.com>
2016-11-24 21:50:55 +0800

    recruit

--- js/index.js
+++ js/index.js
@@ -2,8 +2,12 @@
 function change(element) {
     var Lis = $('.'+element+' .head-title li');
     var hContent = $('.' + element + ' .h-content');
+    var bluebar = $('.' + element + ' .bluebar');
     Lis.click(function () {
-       $(this).addClass('on').siblings().removeClass('on');
+        var num = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+        bluebar.animate({left:$(this).width()*num});
+        console.log(bluebar.offset().left)
         hContent.eq($(this).index()).addClass('changetab').siblings().removeClass('changetab');
    })
 }
@@ -11,6 +15,36 @@ change('information');
 change('actual');
 change('market');
 change('business');
+/*********************search*************************/
+(function () {
+    var input = $('.header-right input');
+    $('.search').toggle(function () {
+        input.animate({width: 280, left: -260});
+    },function () {
+        input.animate({width: 0, left: 0});
+    });
+    input.focus(function () {
+        $(this).css({borderBottom:'1px solid #959798'})
+    });
+    input.blur(function () {
+        $(this).css({borderBottom:'1px solid #666'})
+    });
+})();
+/***********sign in/on**************model*****************/
+var model = $('.model');
+var modelcantainer = $('model-container');
+$('.signin').click(function () {
+    model.css({display: 'block'}).animate({opacity: 1},300);
+});
+$('.signup').click(function () {
+    model.animate({});
+
+});
+model.click(function () {
+    $(this).css({display:'none',opacity: 0})
+});
+
+
 /******************artical*********************/
 $('.content .left .title h2').click(function () {
     $(this).addClass('on').siblings().removeClass('on');
@@ -26,4 +60,66 @@ function time() {
     $('.calendar .c-time').html('<span><i class="iconfont icon-shijian"></i>'+calendarTime+'</span><span>据下次数据公布时间还有:<em>1小时4分17秒</em></span>')
 }
 setInterval(time, 1000);
-/***************************************************/
+/***********************notice垂直轮播图****************************/
+function verticalscroll(container,time) {
+    var lists = $(container);
+    var firstLi = $(container+' p:last');
+    lists.prepend(firstLi.clone(true));
+    var lis = lists.children();
+    lists.css({marginTop: -45 * (lis.length-1)});
+    var index = lis.length;
+    var sid = setInterval(function () {
+        index--;
+        if (index<0) {
+            lists.css({marginTop: -45 * (lis.length-1)});
+            index = lis.length-1;
+        }
+        lists.animate({marginTop:-index*45},1000)
+    }, time);
+}
+verticalscroll('.left .scroll-box-list',2200);
+verticalscroll('.right .scroll-box-list',3500);
+
+/**************************************************************/
+(function () {
+    var html = '';
+    for (var i = 0; i < indexdata.newest.length; i++) {
+        var newest = indexdata.newest[i];
+        html += '<li><a href="#"><div class="img"><img src="' + newest.titlepic + '" /></div><div><h3>' + newest.title + '</h3><p>' + newest.content + '</p></div></a><div class="author"><a href="#"><img src="' + newest.editorpic + '"/><span>' + newest.editor + '</span></a><em class="iconfont icon-shijian"> ' + newest.time + '</em></div></li>';
+    }
+    $('.newest').html(html);
+})();
+    function sort(container) {
+        var html = '';
+        for (var i=0;i<indexdata.hottest[container].length;i++) {
+            var hot = indexdata.hottest[container][i];
+            html+='<li><i>'+(i+1)+'</i><a href="#">'+hot.title+'</a><p><span class="iconfont icon-luntanzhongxin"></span>'+hot.discuss+'<i>|</i><em class="iconfont icon-yanjing"></em>'+hot.quantity+'</p></li>';
+        }
+        $('.' + container + ' ul').html(html);
+    }
+    sort('daysort');
+    sort('weeksort');
+(function () {
+    var html = '';
+    for (var i=0;i<indexdata.community.length;i++) {
+        var community = indexdata.community[i];
+        html += '<li><a href="#"><div class="img"><img src="'+community.pic+'"/></div><p>'+community.title+'</p></a><span><em class="iconfont icon-luntanzhongxin"></em> '+community.num+'</span></li>';
+    }
+    $('.middle ul').html(html);
+})();
+(function () {
+    var html = '';
+    for (var i=0;i<indexdata.author.length;i++) {
+        var author = indexdata.author[i];
+        html += '<li><div class="people clearfix"><a href="#"><img src="'+author.pic+'"/><span>'+author.name+'</span></a><i>'+author.fans+'</i></div><a class="content" href="#">'+author.artical+'</a><div class="follow">+关注</div></li>';
+    }
+    $('.right ul').html(html);
+})();
+(function () {
+    var html = '';
+    for (var i=0;i<indexdata.banner.length;i++) {
+        var banner = indexdata.banner[i];
+        html += '<li><a href=""><div class="transi"></div><img class="first" src="'+banner.pic+'"/><span>'+banner.title+'</span></a></li>';
+    }
+    $('.banner ul').html(html);
+})();
---
+++ js/indexdata.js
@@ -0,0 +1,414 @@
+var indexdata = {
+    "stockIndex":{},
+    "banner":[{
+        "pic":"img/data/banner/1.jpg",
+        "title":"离岸人民币大跌后反弹 收复6.95关口"
+    },{
+        "pic":"img/data/banner/2.jpg",
+        "title":"30年债牛拐点真的来临？美国国债再度暴跌"
+    },{
+        "pic":"img/data/banner/3.jpg",
+        "title":"美联储11月纪要：绝大多数委员认为加息应相对快地到来"
+    },{
+        "pic":"img/data/banner/4.jpg",
+        "title":"隔夜市场惊心动魄 60亿美元卖单引发黄金狂跌"
+    },{
+        "pic":"img/data/banner/5.jpg",
+        "title":"全面解读表外监管新规：理财增速或放缓 警惕“资金荒”"
+    },],
+    "newest":[{
+        "title":"意大利公投！欧洲的下一个“黑天鹅”即将来袭",
+        "content":"意大利修宪公投将于12月4日举行，最后一份民调显示反对派领先约10个百分点，或引发伦齐下台与提前大选。分析认为本次公投或导致更广泛的欧元区危机，该国银行难以脱离困境，欧洲金融系统稳定性或被削弱。",
+        "titlepic":"img/data/newestpic/1.jpg",
+        "editor":"刘怡心",
+        "editorpic":"img/data/editorpic/1.jpg",
+        "time":"4分钟前"
+    },{
+        "title":"沪铜沪锌盘中涨停 黑色系远月合约涨势惊人",
+        "content":"内盘期货大多品种今日再度上涨，沪铜、沪锌一度涨停，黑色系主力合约则出现分化走势，但远月合约涨势惊人。",
+        "titlepic":"img/data/newestpic/2.jpg",
+        "editor":"张家玮",
+        "editorpic":"img/data/editorpic/2.jpg",
+        "time":"6分钟前"
+    },{
+        "title":"定增取消传闻令市场炸开了锅 真相到底是什么？",
+        "content":"内盘期货大多品种再度上涨，沪铜、沪锌一度涨停，黑色系主力合约走势分化，但铁矿石、焦煤、焦炭多个远月合约涨停或接近涨停。昨日，山西焦煤集团与六大钢铁集团签订了2017年中长期合同。",
+        "titlepic":"img/data/newestpic/3.jpg",
+        "editor":"顾雪琳",
+        "editorpic":"img/data/editorpic/3.jpg",
+        "time":"7分钟前"
+    },{
+        "title":"全面解读表外监管新规：理财增速或放缓 警惕“资金荒”",
+        "content":"中国银监会昨日修订《商业银行表外业务风险管理指引》，拟全面监管表外业务，分析师多认为这将引发表外业务增速放缓，资产荒或变为资金荒，对债市而言中长期影响偏负面，企业信用风险或因此爆发。",
+        "titlepic":"img/data/newestpic/4.jpg",
+        "editor":"祁月",
+        "editorpic":"img/data/editorpic/4.jpg",
+        "time":"12分钟前"
+    },{
+        "title":"隔夜市场惊心动魄 60亿美元卖单引发黄金狂跌",
+        "content":"隔夜黄金因价值超60亿美元的卖单而跌破1200美元/盎司。美债遭大幅抛售，欧元和欧洲股市下跌，离岸人民币跌破6.95刷新历史最低。而美元指数升至近十四年新高，道指、标普收盘均刷新记录新高。",
+        "titlepic":"img/data/newestpic/5.jpg",
+        "editor":"石惠",
+        "editorpic":"img/data/editorpic/5.jpg",
+        "time":"16分钟前"
+    },{
+        "title":"【周三美股总结】道指、标普500、罗素2000再创新高 科技股拖累纳指下跌",
+        "content":"美国耐用品订单和消费者信心指数表现强劲推高工业股，卡特彼勒涨约2.7%、联合技术公司涨逾1%。其他个股方面，农业机械巨头Deere涨11%，礼来跌10%，FB跌0.5%。携程财报强劲，盘后一度涨超9%。",
+        "titlepic":"img/data/newestpic/6.jpg",
+        "editor":"顾雪琳",
+        "editorpic":"img/data/editorpic/3.jpg",
+        "time":"40分钟前"
+    },{
+        "title":"【黄金晨报】美联储“锁定”加息美元暴涨 黄金大跌30美元创九个月新低",
+        "content":"黄金周三（11月23日）美盘大跳水30美元，收于1190美元下方，创九个月新低。FOMC纪要基本基本确定12月加息，美元创14年新高，美股连续第三天刷新历史新高，黄金ETF基金连续十天抛售持仓，金价创两周来最大单日跌幅。",
+        "titlepic":"img/data/newestpic/7.jpg",
+        "editor":"一财网",
+        "editorpic":"img/data/editorpic/6.png",
+        "time":"45分钟前"
+    },{
+        "title":"不良贷款格局分化：从东部沿海向中西部和东北地区转移",
+        "content":"据多地银监局最新数据显示，不良贷款东部双降西部双升。甘肃等西部地区不良双升，黑龙江、吉林、河南等省份不良率仍高达3%以上，表现出从东南沿海向内陆地区蔓延的趋势。伴随不良资产的区域间转移，各地地方资管公司设立迎来高峰。",
+        "titlepic":"img/data/newestpic/8.jpg",
+        "editor":"张家玮",
+        "editorpic":"img/data/editorpic/2.jpg",
+        "time":"58分钟前"
+    },{
+        "title":"30年债牛拐点真的来临？美国国债再度暴跌",
+        "content":"10年期美债价格周三下跌，收益率一度飙升10个基点，为大选以来最大涨幅，最高升至2.42%，创去年7月以来新高。昨日公布的美国耐用品订单和初请失业金良好，加强经济和通胀向好预期。",
+        "titlepic":"img/data/newestpic/9.jpg",
+        "editor":"刘怡心",
+        "editorpic":"img/data/editorpic/1.jpg",
+        "time":"1小时前"
+    },{
+        "title":"月批改周批 IPO加速暗推注册制向前",
+        "content":"随着IPO月批改周批，下半年以来获批企业数量和融资规模均超上半年的两倍，“堰塞湖”明显消解，成功铺路注册制改革。18日，人大常委吴晓灵指出明年将进一步推进注册制改革。",
+        "titlepic":"img/data/newestpic/10.jpg",
+        "editor":"张家玮",
+        "editorpic":"img/data/editorpic/2.jpg",
+        "time":"2小时前"
+    },{
+        "title":"融创中国孙宏斌：本轮调控严厉程度超过过往历次 地产业再增长已很难",
+        "content":"融创中国董事长孙宏斌认为，本轮宏观调控严厉程度超过过往，房地产行业再增长已很难，集中度会更高；资金流动性仍充裕，经济下行必会货币宽松；即便经济下行压力较大，国内也可找到发展机遇，始终看好房地产发展。",
+        "titlepic":"img/data/newestpic/11.jpg",
+        "editor":"顾雪琳",
+        "editorpic":"img/data/editorpic/3.jpg",
+        "time":"2小时前"
+    },{
+        "title":"美国怎么看 中国买不买？",
+        "content":"姜超称，纵观中美，短期来看资本市场都对美股、A股充满信心，主要因为再通胀和经济复苏预期，以及趋势交易力量。但无论是特朗普刺激计划、还是中国地产基建的驱动模式都有争议，同时利率上升或会导致资本市场失去宽松货币政策的保护。",
+        "titlepic":"img/data/newestpic/12.jpg",
+        "editor":"祁月",
+        "editorpic":"img/data/editorpic/4.jpg",
+        "time":"2小时前"
+    },{
+        "title":"美联储临近加息 道指、标普均创收盘历史新高",
+        "content":"11月FOMC会议纪要显示，绝大多数官员认为加息应该相对快地到来。今日，道指和标普均创下历史新高。罗素2000指数收涨0.58%，连续第14个交易日上涨，创1996年以来最长连涨天数。",
+        "titlepic":"img/data/newestpic/13.jpg",
+        "editor":"刘怡心",
+        "editorpic":"img/data/editorpic/5.jpg",
+        "time":"2小时前"
+    },{
+        "title":"油价回升刺激产油复苏 美国活跃石油钻台数创年内新高",
+        "content":"本周美国活跃石油钻井平台增至474台，数量创今年1月以来新高。稍早公布的上周美国EIA原油库存意外减少，加之伊拉克总理称愿加入减产，WTI油价一度涨0.5%。本周初高盛四年多来首度唱多大宗商品，上调WTI预期价。",
+        "titlepic":"img/data/newestpic/14.jpg",
+        "editor":"一财网",
+        "editorpic":"img/data/editorpic/6.png",
+        "time":"2小时前"
+    },{
+        "title":"美联储11月纪要：绝大多数委员认为加息应相对快地到来",
+        "content":"北京时间周四凌晨，美联储公布11月会议纪要。绝大多数官员认为劳动力市场闲置减少，加息应该相对快地到来。有官员直接指出，12月加息合适。《华尔街日报》称，纪要显示美联储倾向于12月加息，强化了市场对加息时点的预期。",
+        "titlepic":"img/data/newestpic/15.jpg",
+        "editor":"一财网",
+        "editorpic":"img/data/editorpic/6.png",
+        "time":"2小时前"
+    },{
+        "title":"伊拉克称愿意加入减产 油价直线上涨",
+        "content":"伊拉克总理今日表示，将承担起OPEC在9月份为该国制定的减产责任，并称优先任务是提高每桶原油的价格。消息一出，原油直线上涨。",
+        "titlepic":"img/data/newestpic/16.jpg",
+        "editor":"刘怡心",
+        "editorpic":"img/data/editorpic/5.jpg",
+        "time":"2小时前"
+    },{
+        "title":"道指何时站上20000点？一张图告诉你",
+        "content":"本周二道指收盘首次升破19000点，次日盘中道指创新高。投行Oppenheimer & Co.的技术分析主管Wald认为，经历去年大幅波动，目前道指已突破阻力位上行，呈典型的牛市表现，明年上半年会涨至20000点。",
+        "titlepic":"img/data/newestpic/17.jpg",
+        "editor":"祁月",
+        "editorpic":"img/data/editorpic/4.jpg",
+        "time":"2小时前"
+    },{
+        "title":"美国经济数据远胜预期 美元大涨 道指创新高",
+        "content":"美元对日元涨至八个多月最高点，欧元对美元跌至去年4月以来最低；道指创盘中最高纪录；金价重挫，美债又遭抛售，10年期美债收益率创去年7月以来新高。稍早公布的美国耐用品订单和消费者信心指数表现均远强于预期。",
+        "titlepic":"img/data/newestpic/18.jpg",
+        "editor":"顾雪琳",
+        "editorpic":"img/data/editorpic/3.jpg",
+        "time":"2小时前"
+    },{
+        "title":"特朗普当选后 美国消费者信心创七个月新高",
+        "content":"美国11月密歇根大学消费者信心指数终值93.8，创5月份以来终值最高。密歇根大学消费调查主管Curtin表示，消费者对特朗普胜选的初步反应是积极的，他们对个人财务和国家经济前景改善感到乐观。",
+        "titlepic":"img/data/newestpic/19.jpg",
+        "editor":"张家玮",
+        "editorpic":"img/data/editorpic/2.jpg",
+        "time":"2小时前"
+    },{
+        "title":"连创纪录新低！离岸人民币逼近6.95 在岸人民币跌破6.91",
+        "content":"夜盘在岸人民币兑美元接连跌破6.90、6.91两个关口，创八年半新低。离岸人民币兑美元接近跌破6.95关口，创历史新低。德商银行周浩表示，只要美元指数保持近期的强势，人民币难以逆转颓势。",
+        "titlepic":"img/data/newestpic/20.gif",
+        "editor":"刘怡心",
+        "editorpic":"img/data/editorpic/1.jpg",
+        "time":"2小时前"
+    }],
+    "hottest":{
+        "daysort":[{
+            "title":"这个传闻一旦坐实 金价恐再跌200美元",
+            "discuss":"2",
+            "quantity":"144718"
+        },{
+            "title":"离岸人民币再创新低 首次跌破6.92",
+            "discuss":"88",
+            "quantity":"128910"
+        },{
+            "title":"连创纪录新低！离岸人民币逼近6.95 在岸人民币跌破6.91",
+            "discuss":"74",
+            "quantity":"77102"
+        },{
+            "title":"中信证券全面看多：泡沫大迁移 楼市资金将流入股市",
+            "discuss":"54",
+            "quantity":"28446"
+        },{
+            "title":"罕见限制存款 印度的富人第一次求着穷人办事",
+            "discuss":"25",
+            "quantity":"24534"
+        },{
+            "title":"人民币中间价2008年6月来首次跌破6.9 离岸跌破6.96",
+            "discuss":"71",
+            "quantity":"23281"
+        },{
+            "title":"【见闻对话】AIG首席经济学家：成天说的去杠杆去了吗？该破产就要破产！",
+            "discuss":"22",
+            "quantity":"22963"
+        },{
+            "title":"早餐FM-Radio|2016年11月24日",
+            "discuss":"3",
+            "quantity":"21170"
+        },{
+            "title":"张斌：不要把人民币多头逼成空头",
+            "discuss":"63",
+            "quantity":"16386"
+        },{
+            "title":"美国10月耐用品订单初值远超预期 黄金急跌 美元飙升",
+            "discuss":"28",
+            "quantity":"15564"
+        }],
+        "weeksort":[{
+            "title":"越来越多迹象显示 中国经济又开始降温了",
+            "discuss":"126",
+            "quantity":"482264"
+        },{
+            "title":"不死心 日本和秘鲁联合宣布继续推进TPP",
+            "discuss":"16",
+            "quantity":"375368"
+        },{
+            "title":"不用担心！川普当选对中国是利好",
+            "discuss":"66",
+            "quantity":"200508"
+        },{
+            "title":"河北钢企大呼“不公平”：去产能是全国性的，不应只由河北承担",
+            "discuss":"48",
+            "quantity":"152481"
+        },{
+            "title":"这个传闻一旦坐实 金价恐再跌200美元",
+            "discuss":"2",
+            "quantity":"144718"
+        },{
+            "title":"如果美元再涨10% 会发生什么？",
+            "discuss":"31",
+            "quantity":"134192"
+        },{
+            "title":"离岸人民币再创新低 首次跌破6.92",
+            "discuss":"88",
+            "quantity":"128910"
+        },{
+            "title":"沙特终于要披露石油市场最大的秘密了",
+            "discuss":"10",
+            "quantity":"113605"
+        },{
+            "title":"今晚，这个女人的讲话或将引发市场巨震！",
+            "discuss":"1",
+            "quantity":"91859"
+        },{
+            "title":"重磅报告！这是高盛眼中的2017年市场十大主题",
+            "discuss":"15",
+            "quantity":"86664"
+        }]
+    },
+    "community":[{
+        "pic":"img/data/community/1.jpg",
+        "title":"美元外流是否相应的减少了RMB的总量？",
+        "num":"57人参与"
+    },{
+        "pic":"img/data/community/2.jpg",
+        "title":"如果大幅搞基建就能救美国，为什么奥巴马不做？",
+        "num":"113人参与"
+    },{
+        "pic":"img/data/community/3.jpg",
+        "title":"为什么人民币狂跌，A股还在往上走？",
+        "num":"104人参与"
+    },{
+        "pic":"img/data/community/4.jpg",
+        "title":"每次美元升值都会给一些国家带来危机，这次会是谁？",
+        "num":"126人参与"
+    },{
+        "pic":"img/data/community/5.jpg",
+        "title":"征集 | 明年怎么投资 这是今年最后一次向大咖们提问的机会",
+        "num":"74人参与"
+    },{
+        "pic":"img/data/community/6.jpg",
+        "title":"原油跟美元之间存在着怎样的关系？",
+        "num":"87人参与"
+    },{
+        "pic":"img/data/community/7.jpg",
+        "title":"中国未来一两年会不会加息？",
+        "num":"42人参与"
+    },{
+        "pic":"img/data/community/8.jpg",
+        "title":"中国房价见顶了么？",
+        "num":"41人参与"
+    },{
+        "pic":"img/data/community/9.jpg",
+        "title":"中国目前的经济政策是宽松型还是紧缩型",
+        "num":"23人参与"
+    },{
+        "pic":"img/data/community/10.jpg",
+        "title":"如果欧盟解体，对中国有什么影响？",
+        "num":"25人参与"
+    },{
+        "pic":"img/data/community/11.jpg",
+        "title":"美国历次加息周期对全球经济的影响？",
+        "num":"27人参与"
+    },{
+        "pic":"img/data/community/12.jpg",
+        "title":"目前形势下，老百姓该如何保值或增值自己的银行存款？",
+        "num":"22人参与"
+    },{
+        "pic":"img/data/community/13.jpg",
+        "title":"保3万亿外汇还是守7的底？",
+        "num":"65人参与"
+    },{
+        "pic":"img/data/community/14.jpg",
+        "title":"未来二十年，农业的蓝海在哪里？",
+        "num":"111人参与"
+    },{
+        "pic":"img/data/community/15.jpg",
+        "title":"个人理财买什么产品最好？",
+        "num":"19人参与"
+    },{
+        "pic":"img/data/community/16.jpg",
+        "title":"高盛顶级交易最快几周全军覆灭？",
+        "num":"23人参与"
+    },{
+        "pic":"img/data/community/17.jpg",
+        "title":"美联储加息时点临近，美指近日破百是否起飞模式已开启？",
+        "num":"29人参与"
+    }],
+    "author":[{
+        "pic":"img/data/author/1.jpg",
+        "name":"姜超",
+        "fans":"粉丝 21880",
+        "artical":"美国怎么看 中国买不买？"
+    },{
+        "pic":"img/data/author/2.jpg",
+        "name":"王涵",
+        "fans":"粉丝 11780",
+        "artical":"大萧条时贸易战的启示（“抢需求”系列报告之一）"
+    },{
+        "pic":"img/data/author/3.jpg",
+        "name":"见智研究所",
+        "fans":"粉丝 2808",
+        "artical":"【见智研究】欧版4万亿已在路上？"
+    },{
+        "pic":"img/data/author/4.jpg",
+        "name":"任泽平",
+        "fans":"粉丝 9781",
+        "artical":"任泽平：中国经济正在“软着陆”"
+    },{
+        "pic":"img/data/author/5.jpg",
+        "name":"荀玉根",
+        "fans":"粉丝 14446",
+        "artical":"上周资金净流出142亿"
+    },{
+        "pic":"img/data/author/6.jpg",
+        "name":"管清友",
+        "fans":"粉丝 91",
+        "artical":"资本推动新产业阶段能获丰厚回报"
+    },{
+        "pic":"img/data/author/7.jpg",
+        "name":"邓海清",
+        "fans":"粉丝 2982",
+        "artical":"债市调整是“中场休息”还是“终点站”？"
+    },{
+        "pic":"img/data/author/8.jpg",
+        "name":"韩会师",
+        "fans":"粉丝 2491",
+        "artical":"韩会师：6.90能成为人民币的新站点吗？"
+    },{
+        "pic":"img/data/author/9.jpg",
+        "name":"黄卓",
+        "fans":"粉丝 14",
+        "artical":"央行征信中心与民营征信企业互为补充，共同助力普惠金融发展"
+    },{
+        "pic":"img/data/author/10.jpg",
+        "name":"李迅雷",
+        "fans":"粉丝 6612",
+        "artical":"钱发多了一定会找地方宣泄"
+    },{
+        "pic":"img/data/author/11.jpg",
+        "name":"凌鹏",
+        "fans":"粉丝 1561",
+        "artical":"会不会“滞胀”？"
+    },{
+        "pic":"img/data/author/12.jpg",
+        "name":"屈庆",
+        "fans":"粉丝 1126",
+        "artical":"利率虽冲高回落 拐点仍言之尚"
+    },{
+        "pic":"img/data/author/13.jpg",
+        "name":"张明",
+        "fans":"粉丝 295",
+        "artical":"国际金融市场近期为何动荡加剧？"
+    },{
+        "pic":"img/data/author/14.jpg",
+        "name":"高善文",
+        "fans":"粉丝 122",
+        "artical":"旬度经济观察 ——商品房成交显著降温，人民币释放高估压力"
+    },{
+        "pic":"img/data/author/15.jpg",
+        "name":"鲁政委",
+        "fans":"粉丝 1692",
+        "artical":"让绿色金融不再只是“情怀”！"
+    },{
+        "pic":"img/data/author/16.jpg",
+        "name":"朱海斌",
+        "fans":"粉丝 79",
+        "artical":"短期下行压力仍大 期待结构性改革获突破"
+    },{
+        "pic":"img/data/author/17.jpg",
+        "name":"李礼辉",
+        "fans":"粉丝 14",
+        "artical":"123"
+    },{
+        "pic":"img/data/author/18.jpg",
+        "name":"郭磊",
+        "fans":"粉丝 175",
+        "artical":"一个积极迹象：地产销售的初步软着陆与供给端的平稳修复"
+    },{
+        "pic":"img/data/author/19.jpg",
+        "name":"洪灏",
+        "fans":"粉丝 1235",
+        "artical":"价格的革命——对全球资产配置的思考"
+    },{
+        "pic":"img/data/author/20.jpg",
+        "name":"巴曙松",
+        "fans":"粉丝 46",
+        "artical":"深港通的意义应放在共同市场大框架下评估分析"
+    }]
+};
